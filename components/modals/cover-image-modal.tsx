a82848a7efc93e8b99c1e6f2bf93653f7f69ba44
Ms0232 <180540106059@darshan.ac.in>
2023-11-03 10:00:11 +0530

    image delete & replace

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -31,7 +31,11 @@ export const CoverImageModal = () => {
 
       const res = await edgestore.publicFiles.upload({
         file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
       });
+
       await update({
         id: params.documentId as Id<"documents">,
         coverImage: res.url,
